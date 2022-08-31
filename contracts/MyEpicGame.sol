9da03cfd9104646cfa9c9fabb6bb4b2b5a9ba049
Ryan-Otoole <otoolerj@gmail.com>
2022-08-31 17:27:29 -0500

    nft functions working properly getting ready to deploy to testNet

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -1,10 +1,102 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.1;
+
+// NFT contract to inherit from.
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+// Helper functions OpenZeppelin provides.
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 import "hardhat/console.sol";
 
-contract MyEpicGame {
-    constructor() {
-        console.log("testing constructor fucntion MyEpicGame");
+import "./libraries/Base64.sol";
+
+contract MyEpicGame is ERC721 {
+
+    struct CharacterAttributes {
+        uint characterIndex;
+        string name;
+        string imageURI;
+        uint hp;
+        uint maxHP;
+        uint attackDamage;
     }
+
+      // The tokenId is the NFTs unique identifier, it's just a number that goes
+      // 0, 1, 2, 3, etc.
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    CharacterAttributes[] defaultCharacters;
+
+    mapping(uint => CharacterAttributes) public nftHolderAttributes;
+
+    mapping(address => uint) public nftHolders;
+
+    constructor(
+        string[] memory characterNames,
+        string[] memory characterImageURIs,
+        uint[] memory characterHP,
+        uint[] memory characterAttackDamage
+    )     ERC721("Animales", "ANIMAL")
+    {
+        for (uint i=0; i < characterNames.length; i++) {
+            defaultCharacters.push(CharacterAttributes({
+                characterIndex: i,
+                name: characterNames[i],
+                imageURI: characterImageURIs[i],
+                hp: characterHP[i],
+                maxHP: characterHP[i],
+                attackDamage: characterAttackDamage[i]
+            }));
+            CharacterAttributes memory c = defaultCharacters[i];
+            console.log("Done initializing %s w/ HP %s, img %s", c.name, c.hp, c.imageURI);
+        }
+        _tokenIds.increment();
+    }
+
+    function mintCharacterNFT(uint _characterIndex) external {
+        uint newItemId = _tokenIds.current();
+        _safeMint(msg.sender, newItemId);
+
+        nftHolderAttributes[newItemId] = CharacterAttributes({
+            characterIndex: _characterIndex,
+            name: defaultCharacters[_characterIndex].name,
+            imageURI: defaultCharacters[_characterIndex].imageURI,
+            hp: defaultCharacters[_characterIndex].hp,
+            maxHP: defaultCharacters[_characterIndex].maxHP,
+            attackDamage: defaultCharacters[_characterIndex].attackDamage
+        });
+
+        console.log("Minted NFT w/ tokenId %s and characterIndex %s", newItemId, _characterIndex);
+
+        nftHolders[msg.sender] = newItemId;
+
+        _tokenIds.increment();
+    }
+
+    function tokenURI(uint _tokenId) public view override returns (string memory) {
+        CharacterAttributes memory charAttributes = nftHolderAttributes[_tokenId];
+        console.log("****", charAttributes.hp, "****");
+        string memory stringHP = Strings.toString(charAttributes.hp);
+        string memory stringMaxHP = Strings.toString(charAttributes.maxHP);
+        string memory stringAttackDamage = Strings.toString(charAttributes.attackDamage);
+
+        string memory json = Base64.encode(
+            abi.encodePacked(
+                '{"name": "',
+                charAttributes.name,
+                ' -- NFT #: ',
+                Strings.toString(_tokenId),
+                '", "description": "This is an NFT that lets people play in the game Metaverse Slayer!", "image": "',
+                charAttributes.imageURI,
+                '", "attributes": [ { "trait_type": "Health Points", "value": ',stringHP,', "max_value":',stringMaxHP,'}, { "trait_type": "Attack Damage", "value": ',
+                stringAttackDamage,'} ]}'
+            )
+        );
+        string memory output = string(abi.encodePacked("data:application/json;base64,", json));
+        return output;
+    }
+
  }
--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,24 @@
 const main = async () => {
     const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
-    const gameContract = await gameContractFactory.deploy();
+    const gameContract = await gameContractFactory.deploy(
+      ["SquirrelMan", "PoodleMoth", "Batboy", "Batman"],       // Names
+      ["https://imgur.com/t/squirrel/4MJbBTY", // Images
+      "https://imgur.com/gallery/8EvG6cN", 
+      "https://imgur.com/gallery/uh5YEz4",
+      "https://imgur.com/gallery/IC4DdQC"
+    ],
+      
+      [100, 200, 300, 175],                    // HP values
+      [100, 50, 25, 125]                       // Attack damage values
+    );
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
+    let txn;
+    txn = await gameContract.mintCharacterNFT(2);
+    await txn.wait();
+
+    let returnedTokenURI = await gameContract.tokenURI(1);
+    console.log('tokenURI', returnedTokenURI);
   };
   
   const runMain = async () => {
