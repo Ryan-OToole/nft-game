478afd782915a5951ad31add97c7e766158a6691
Ryan-Otoole <otoolerj@gmail.com>
2022-09-20 05:41:46 -0500

    going to move contract logic back into solidity side not able to call it from js side due to permission issues

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -1,3 +1,4 @@
+
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.1;
 
@@ -192,6 +193,6 @@ contract MyEpicGame is ERC721 {
     }
 
     function getAllPlayersInGame() public view returns (CharacterAttributes[] memory) {
-        return allPlayersInGame;
+  
     }
  }
